2a7f7a8fd61f314599b058a1766d7b47e927b96e
Satdinov <satdinov.rsh@yandex.ru>
2022-05-25 17:11:00 +0400

    query = User.query.order_by(User.id.desc())

--- database/loaders/users_loaders.py
+++ database/loaders/users_loaders.py
@@ -1,19 +1,15 @@
 from datetime import datetime
 from enum import Enum
 from typing import Optional
-
 from gino.api import GinoExecutor
-
 from ..models import User,UserLang
-
-
 
 def users_query(
     user_id: Optional[str] = None,
     email: Optional[str] = None,
     lang: Optional[UserLang] = None,
 ) -> GinoExecutor:
-    query = User.query
+    query = User.query.order_by(User.id.desc())
 
     if user_id:
         query = query.where(User.id == user_id)
